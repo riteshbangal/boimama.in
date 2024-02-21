b42b6a596177d0d524ca82b4226c01a388a13500
Ritesh Bangal <riteshbangal@gmail.com>
2024-02-21 22:10:04 +0100

    Frontend Changes: Fix theme button issue

--- frontend/user-portal/assets/scripts/story.js
+++ frontend/user-portal/assets/scripts/story.js
@@ -1,33 +1,10 @@
 "use strict";
 
-import { loadmoreStories, renderLoadingScreen, buildStoryHTML } from './DataService.js'
-
-// Changes for 'scroll to the top button'
-let scrollTop = document.getElementById("top");
-
-let html = document.querySelector("html");
-let menu = document.querySelector("#menu");
-let header = document.querySelector(".header");
+import { renderLoadingScreen, buildStoryHTML } from './DataService.js'
 
 const init = function () {
     renderLoadingScreen(); // Waiting while loading server data.
     buildStoryHTML(); // API call, fetch data and load HTML content for a single story
-  
-    menu.onclick = function toggleHeader() {
-      menu.classList.toggle("fa-times");
-      header.classList.toggle("toggle");
-    };
-  
-    window.onscroll = function scrollWindow() {
-      menu.classList.remove("fa-times");
-      header.classList.remove("toggle");
-  
-      if (window.scrollY > 50) {
-        scrollTop.style.display = "block";
-      } else {
-        scrollTop.style.display = "none";
-      }
-    };  
   };
   init();
   
